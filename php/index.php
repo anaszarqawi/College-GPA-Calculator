803aa23a549d3170f83870cdaedea5e4155737f9
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2021-07-27 09:59:17 +0200

    styling nav-bar
    -Navigation bar style added

--- php/index.php
+++ php/index.php
@@ -9,14 +9,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
-        <?php include '../css/semester/style.css' ?><?php include '../css/Options/style.css' ?><?php include '../css/GPA/style.css' ?>
+        <?php include '../css/semester/style.css' ?><?php include '../css/Options/style.css' ?><?php include '../css/GPA/style.css' ?><?php include '../css/nav-bar/style.css' ?>
     </style>
 </head>
 
 <body>
+    <?php include './navigationBar.php' ?>
+
     <div id="container">
         <div class="semesters">
             <?php include './semester.php' ?>
