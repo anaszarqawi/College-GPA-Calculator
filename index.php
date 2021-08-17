81007b34e8c5bd470393e87d0f9518ae7a48c320
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2021-08-17 14:36:35 +0200

    edit places files

--- index.php
+++ index.php
@@ -12,29 +12,27 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
-        <?php include '../../css/semester/style.css' ?><?php include '../../css/Options/style.css' ?><?php include '../../css/GPA/style.css' ?><?php include '../../css/nav-bar/style.css' ?>
+        <?php include './css/GPA/style.css' ?><?php include './css/Options/style.css' ?><?php include './css/semester/style.css' ?>
     </style>
 </head>
 
 <body>
-    <?php include '../navigationBar.php' ?>
-
     <div id="container">
         <div class="semesters">
-            <?php include './semester.php' ?>
+            <?php include './php/semester.php' ?>
         </div>
         <button class="add-semester" title="Add another Semester">Add Semester</button>
         <div class="totalSemester">
             <div class="Total_GPA">
                 <span>Total GPA</span>
             </div>
-            <?php include './GPA.php' ?>
+            <?php include './php/GPA.php' ?>
 
         </div>
         <script>
-            <?php include '../../js/jquery-3.6.0.min.js' ?>
-            <?php include '../../js/shake-element-transform/shake.js' ?>
-            <?php include '../../js/script.js' ?>
+            <?php include './js/jquery-3.6.0.min.js' ?>
+            <?php include './js/shake-element-transform/shake.js' ?>
+            <?php include './js/script.js' ?>
         </script>
 </body>
 
