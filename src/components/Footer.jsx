ed06e6b39cecf144ef274f2be84d1deb70dce271
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2022-05-13 22:27:35 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
         <p className="copyright">Made with <span className="heart">🤍</span> by <a href="https://github.com/Anaszarqawi"> @anaszarqawi_</a></p>
-        <a href="https://github.com/Anaszarqawi/College-GPA-Calculator" target="_blank" className="github" rel="noreferrer">Github | v1.0.2</a>
+        <a href="https://github.com/Anaszarqawi/College-GPA-Calculator" target="_blank" className="github" rel="noreferrer">Github | v2.0.0</a>
         </footer>
 
     )
