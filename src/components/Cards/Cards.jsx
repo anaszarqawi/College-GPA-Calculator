a660d209a8e9a915bdb6da80f0fbf2a4c842ead9
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2023-02-02 03:08:07 +0200

    add card componente

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './style.scss';
+import { useCalc } from '../../contexts/calcContext';
+import Card from './Components/Card/card';
 
-const Cards = () => {
-  return <div className="Cards"></div>;
+const Cards = (props) => {
+  const { semesters } = useCalc();
+
+  return (
+    <div className="Cards">
+      {semesters.map((semester) => {
+        return <Card semester={semester} />;
+      })}
+    </div>
+  );
 };
 
 export default Cards;
